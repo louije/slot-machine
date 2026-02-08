ceaaead2cf1abe3063746d6f0488e696fb3c3b70
Louis-Jean Teitelbaum <ljt@meidosem.com>
2026-02-08 11:03:48 +0100

    speed up testagent: 200ms interval instead of 1s

--- spec/testagent/main.go
+++ spec/testagent/main.go
@@ -23,7 +23,8 @@ func main() {
 	_ = flag.String("cwd", "", "working directory")
 	_ = flag.String("system-prompt", "", "system prompt")
 	_ = flag.String("allowedTools", "", "allowed tools")
-	duration := flag.Int("duration", 10, "seconds to run")
+	interval := flag.Int("interval", 200, "milliseconds between events")
+	duration := flag.Int("duration", 10, "number of events to emit")
 	flag.Parse()
 
 	sessionID := fmt.Sprintf("test-session-%d", time.Now().UnixNano())
@@ -34,9 +35,9 @@ func main() {
 	// Init event.
 	fmt.Fprintf(os.Stdout, "{\"type\":\"system\",\"subtype\":\"init\",\"session_id\":\"%s\"}\n", sessionID)
 
-	// Assistant events every second.
+	// Assistant events at interval.
 	for i := 0; i < *duration; i++ {
-		time.Sleep(1 * time.Second)
+		time.Sleep(time.Duration(*interval) * time.Millisecond)
 		fmt.Fprintf(os.Stdout, "{\"type\":\"assistant\",\"text\":\"working on: %s (%d/%d)\"}\n", *prompt, i+1, *duration)
 	}
 
